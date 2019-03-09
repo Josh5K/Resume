00e43aeb4789bf7ec5d1d1d1974ba6a1369374c5
Josh5k <jking57@academic.rrc.ca>
2019-03-09 17:26:43 -0600

    Redirecting root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: redirect('/resume/josh+king')
   get 'resume/:person', to: 'resume#index'
   get 'resume/:person/portfolio', to: 'portfolio#index'
 end
